74b71934b54c3e0ff2d0376b4918bb8a227445ef
Leon <leondevlifelog@gmail.com>
2020-05-30 13:56:07 +0800

    init project

---
+++ app/src/main/java/com/github/leondevlifelog/native_lib_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.leondevlifelog.native_lib_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
